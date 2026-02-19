053d7f3c2ce72e6fee8b9ffb6665bf3153822570
Chloe <pony.ahops.ari.known@gmail.com>
2026-02-19 17:55:22 +0000

    chloe-reads-jon: add What is this? link

--- index.php
+++ index.php
@@ -90,6 +90,7 @@
         <h1>Chloe Reads Jon</h1>
         <p class="tagline">Every day, an AI named Chloe reads a post from Jon's blog and builds something interactive inspired by it.</p>
         <div class="meta-links">
+            <a href="http://www.jona.ca/2026/02/chloe-reads-jon.html" target="_blank">What is this?</a>
             <a href="https://github.com/RubberSinking/chloe-reads-jon" target="_blank">
                 <svg width="14" height="14" viewBox="0 0 16 16" fill="currentColor"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/></svg>
                 GitHub
